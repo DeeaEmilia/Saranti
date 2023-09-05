2a4356ff89a2acfd9c6404056bcc94653704e10e
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-09-06 01:04:28 +0300

    trying to fix images

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import img3 from "../assets/img3.png";
+import img1 from "../assets/img1.png";
+import img7 from "../assets/img7.png";
+import img5 from "../assets/img5.png";
 
 const AboutPage = () => {
   return (
     <main className="about-us__container">
       <h1>About Us</h1>
       <section className="about-us__section">
-        <img src="../src/assets/img3.png" alt="" />
+        <img src={img3} alt="" />
         <h2>About me</h2>
         <p>
           About me bla bla Lorem ipsum dolor sit amet consectetur adipisicing
@@ -17,7 +21,7 @@ const AboutPage = () => {
       </section>
 
       <section className="about-us__section">
-        <img src="../src/assets/img1.png" alt="" />
+        <img src={img1} alt="" />
         <h2>About me</h2>
         <p>
           About me bla bla Lorem ipsum dolor sit amet consectetur adipisicing
@@ -29,7 +33,7 @@ const AboutPage = () => {
       </section>
 
       <section className="about-us__section">
-        <img src="../src/assets/img7.png" alt="" />
+        <img src={img7} alt="" />
         <h2>About me</h2>
         <p>
           About me bla bla Lorem ipsum dolor sit amet consectetur adipisicing
@@ -41,7 +45,7 @@ const AboutPage = () => {
       </section>
 
       <section className="about-us__section">
-        <img src="../src/assets/img5.png" alt="" />
+        <img src={img5} alt="" />
         <h2>About me</h2>
         <p>
           About me bla bla Lorem ipsum dolor sit amet consectetur adipisicing
