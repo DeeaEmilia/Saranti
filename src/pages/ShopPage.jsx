e649d2e9b556580fda232a6e58b318bfe227253c
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-09-07 15:57:05 +0300

    added shop page

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { sanityClient } from "../helpers/client";
+import Product from "../components/Product";
+
+async function getSanityData() {
+  const productQuery = '*[_type == "product"]';
+  const products = await sanityClient.fetch(productQuery);
+  return [products];
+}
 
 const ShopPage = () => {
-  return <div>ShopPage</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getSanityData().then((data) => {
+      setProducts(data[0]);
+    });
+  }, []);
+  return (
+    <div>
+      <div className="shop__products-container">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ShopPage;
